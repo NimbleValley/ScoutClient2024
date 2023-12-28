0b186afeb0a16ab19a9abe92ef0ddd65eab7ffe3
Mason <mason.d.mcmanus@outlook.com>
2023-12-28 17:29:15 -0600

    Removed 2023 features, began updating for 2024

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Tries to get user in fullscreen, probably doesn't work
 function enterFullScreen(element) {
     if (element.requestFullscreen) {
         element.requestFullscreen();
@@ -10,178 +11,107 @@ function enterFullScreen(element) {
     }
 };
 
+// Sections array, each tab
 var sections = document.getElementsByClassName("section");
 
-window.addEventListener("resize", () => {
-    windowResized();
-});
-
-function windowResized() {
-    if (window.innerWidth > window.innerHeight) {
-        for (var i = 0; i < sections.length; i++) {
-            //sections[i].style.scale = "0.5";
-        }
-    }
-}
-
+// Tries to get user in fullscreen
 enterFullScreen(window);
 
+// Animation timeline variable
 var tl = new TimelineMax();
 
-for (var i = 0; i < sections.length; i++) {
+// Hide all sections, except the first
+for (let i = 0; i < sections.length; i++) {
     sections[i].style.display = "none";
 }
+// TODO set this for debugging to set default screen
 sections[0].style.display = "flex";
 
 
+// Alliance select element
 const allianceSelect = document.getElementById("alliance-select");
 
 
-const gridContainer = document.getElementById("grid-container");
-for (var s = 0; s < 3; s++) {
-    var tempSection = document.createElement("div");
-    tempSection.className = "grid-section";
-    if (s == 1) {
-        tempSection.style.backgroundColor = "rgb(78, 74, 74)";
-    }
-    for (var r = 0; r < 3; r++) {
-        var tempRow = document.createElement("div");
-        tempRow.className = "grid-row-auto";
-        for (var n = 0; n < 3; n++) {
-            var tempNode = document.createElement("div");
-            tempNode.className = "grid-node-auto";
-            tempNode.classList.add(0);
-            tempNode.id = (s * 9) + (r * 3) + (n);
-            tempNode.onclick = function () { setAutoNode(this.id) }
-            tempRow.appendChild(tempNode);
-        }
-        tempSection.appendChild(tempRow);
-    }
-    gridContainer.appendChild(tempSection);
-}
-var grids = document.getElementsByClassName("grid-container-full");
-var autoGridData = [];
-
-
-const teleChargeContainer = document.getElementById("tele-charge-container");
-
-
-const teleGridContainer = document.getElementById("tele-grid-container");
-/*for (var s = 0; s < 3; s++) {
-    var tempSection = document.createElement("div");
-    tempSection.className = "grid-section";
-    if (s == 1) {
-        tempSection.style.backgroundColor = "rgb(78, 74, 74)";
-    }
-    for (var r = 0; r < 3; r++) {
-        var tempRow = document.createElement("div");
-        tempRow.className = "grid-row";
-        for (var n = 0; n < 3; n++) {
-            var tempNode = document.createElement("grid-node");
-            tempNode.className = "grid-node-tele";
-            tempNode.classList.add(0);
-            tempNode.id = (s * 9) + (r * 3) + (n);
-            tempNode.onclick = function () { setAutoNode(this.id) }
-            tempRow.appendChild(tempNode);
-        }
-        tempSection.appendChild(tempRow);
-    }
-    teleGridContainer.appendChild(tempSection);
-}*/
-var teleGridData = [];
-
-for (var i = 0; i < 27; i++) {
-    autoGridData[i] = 0;
-}
-
-for (var i = 0; i < 27; i++) {
-    teleGridData[i] = 0;
-}
-setUpTeleGrid();
-
-
+// Auto dropped pieces
+// TODO might need to removed in 2024
 var autoDroppedPieced = 0;
 const autoDroppedPiecesText = document.getElementById("auto-dropped=pieces-text");
 
-
+// Tele dropped pieces
+// TODO might need to removed in 2024
 var teleDroppedPieced = 0;
 const teleDroppedPiecesText = document.getElementById("tele-dropped=pieces-text");
 
 
+// Auto mobility
+// TODO might need to removed in 2024
 var autoMobility = false;
 const autoMobilityCheck = document.getElementById("auto-mobility-check");
 
 
-
+// Auto tech
 var autoTech = false;
 const autoTechCheck = document.getElementById("auto-tech-check");
 
 
-
-var autoCharge = false;
-const autoChargeCheck = document.getElementById("auto-charge-check");
-
-const autoChargeOutcomeSelect = document.getElementById("auto-charge-container");
-autoChargeOutcomeSelect.style.display = "none";
-
-
-
+// Pickup method
+// TODO might need to removed in 2024
 const pickupMethodSelect = document.getElementById("pickup-method");
 
 
-
+// Tele tech
 var teleTech = false;
 const teleTechCheck = document.getElementById("tele-tech-check");
 
 
-
+// Flipped
 var teleFlip = false;
 const teleFlipCheck = document.getElementById("tele-flip-check");
 
 
-
+// Comms issues
 var teleComms = false;
 const teleCommsCheck = document.getElementById("tele-comms-check");
 
 
-
+// Disabled during match
 var teleDisabled = false;
 const teleDisabledCheck = document.getElementById("tele-disabled-check");
 
 
-
-var teleCharge = false;
-const teleChargeCheck = document.getElementById("tele-charge-check");
-const teleChargeOutcomeSelect = document.getElementById("tele-charge");
-teleChargeContainer.style.display = "none";
-
-
-
+// Tele parked
+// TODO might need to removed in 2024
 var telePark = false;
 const teleParkCheck = document.getElementById("tele-park-check");
 
 
-
+// Were they dumb
 var dumb = false;
 const dumbCheck = document.getElementById("dumb-check");
 
 
-
+// Were they reckless
 var reckless = false;
 const recklessCheck = document.getElementById("reckless-check");
 
 
-
+// Was the auto successful
 var autoSuccess = true;
 
 
-
+// Which section user is on
 var onSection = 0;
 
-
-
-
+// Switched section user is on from current to next
 async function switchSection(current, next) {
+    if(next == -1) {
+        return;
+    }
+
+    if(next > 5) {
+        return;
+    }
+
     if(next > current) {
         onSection ++;
     } else {
@@ -194,125 +124,23 @@ async function switchSection(current, next) {
     });
 
     if (next > current) {
-        tl.to(sections[current], { transform: "translate(-50vw, -50vh) scale(0.5) rotate(-45deg)", opacity: 0, duration: 0.5, ease: "power2" });
+        tl.to(sections[current], { transform: "translate(-60vw, 0vh) scale(0.45) rotate(-7.5deg)", opacity: 0, duration: 0.5, ease: "power2" });
     } else {
-        tl.to(sections[current], { transform: "translate(50vw, 50vh) scale(0.5) rotate(45deg)", opacity: 0, duration: 0.5, ease: "power2" });
+        tl.to(sections[current], { transform: "translate(60vw, 0vh) scale(0.45) rotate(7.5deg)", opacity: 0, duration: 0.5, ease: "power2" });
     }
 
     sections[next].style.display = "flex";
 
     if (next > current) {
-        tl.fromTo(sections[next], { transform: "translate(50vw, 50vh) scale(0.5) rotate(45deg)", opacity: 0 }, { transform: "", opacity: 1, duration: 0.5, ease: "power2" }, "-=0.35");
+        tl.fromTo(sections[next], { transform: "translate(60vw, 0vh) scale(0.45) rotate(7.5deg)", opacity: 0 }, { transform: "", opacity: 1, duration: 0.5, ease: "power2" }, "-=0.35");
     } else {
-        tl.fromTo(sections[next], { transform: "translate(-50vw, -50vh) scale(0.5) rotate(-45deg)", opacity: 0 }, { transform: "", opacity: 1, duration: 0.5, ease: "power2" }, "-=0.35");
+        tl.fromTo(sections[next], { transform: "translate(-60vw, 0vh) scale(0.45) rotate(-7.5deg)", opacity: 0 }, { transform: "", opacity: 1, duration: 0.5, ease: "power2" }, "-=0.35");
     }
     await sleep(500);
     sections[current].style.display = "none";
-
-    windowResized();
 }
 
-function setAutoNode(id) {
-    var node = document.getElementsByClassName("grid-node-auto")[id];
-    if (id == 0 || id == 1 || id == 2 || id == 9 || id == 10 || id == 11 || id == 18 || id == 19 || id == 20) {
-        autoGridData[id] = autoGridData[id] + 1;
-        if (autoGridData[id] > 2) {
-            autoGridData[id] = 0;
-        }
-
-        if (autoGridData[id] == 1) {
-            node.style.backgroundColor = "#6643DA";
-        } else if (autoGridData[id] == 2) {
-            node.style.backgroundColor = "#FDC955";
-        } else {
-            node.style.backgroundColor = "rgb(196, 190, 184)";
-        }
-    } else {
-        if (autoGridData[id] == 0) {
-            if ((id + 2) % 3 == 0) {
-                autoGridData[id] = 1;
-                node.style.backgroundColor = "#6643DA";
-            } else {
-                autoGridData[id] = 2;
-                node.style.backgroundColor = "#FDC955";
-            }
-        } else {
-            autoGridData[id] = 0;
-            node.style.backgroundColor = "rgb(196, 190, 184)";
-        }
-    }
-    tl.to(node, { scale: 1.25, duration: 0.15, ease: "power2" });
-    tl.to(node, { scale: 1, duration: 0.15, ease: "power2" });
-}
-
-function setTeleNode(id) {
-    var node = document.getElementsByClassName("grid-node-tele")[id];
-    if (id == 0 || id == 1 || id == 2 || id == 9 || id == 10 || id == 11 || id == 18 || id == 19 || id == 20) {
-        teleGridData[id] = teleGridData[id] + 1;
-        if (teleGridData[id] > 2) {
-            teleGridData[id] = 0;
-        }
-
-        if (teleGridData[id] == 1) {
-            node.style.backgroundColor = "#6643DA";
-        } else if (teleGridData[id] == 2) {
-            node.style.backgroundColor = "#FDC955";
-        } else {
-            node.style.backgroundColor = "rgb(196, 190, 184)";
-        }
-    } else {
-        if (teleGridData[id] == 0) {
-            if ((id + 2) % 3 == 0) {
-                teleGridData[id] = 1;
-                node.style.backgroundColor = "#6643DA";
-            } else {
-                teleGridData[id] = 2;
-                node.style.backgroundColor = "#FDC955";
-            }
-        } else {
-            teleGridData[id] = 0;
-            node.style.backgroundColor = "rgb(196, 190, 184)";
-        }
-    }
-    tl.to(node, { scale: 1.25, duration: 0.15, ease: "power2" });
-    tl.to(node, { scale: 1, duration: 0.15, ease: "power2" });
-}
-
-function setUpTeleGrid() {
-
-    console.log(autoGridData);
-    teleGridContainer.innerHTML = "";
-    for (var s = 0; s < 3; s++) {
-        var tempSection = document.createElement("div");
-        tempSection.className = "grid-section";
-        if (s == 1) {
-            tempSection.style.backgroundColor = "rgb(78, 74, 74)";
-        }
-        for (var r = 0; r < 3; r++) {
-            var tempRow = document.createElement("div");
-            tempRow.className = "grid-row";
-            for (var n = 0; n < 3; n++) {
-                var tempNode = document.createElement("div");
-                tempNode.className = "grid-node-tele";
-                tempNode.classList.add(0);
-                tempNode.id = (s * 9) + (r * 3) + (n);
-                if (autoGridData[(s * 9) + (r * 3) + (n)] != 0) {
-                    tempNode.style.border = "orangered solid 1.5vw";
-                    if (autoGridData[(s * 9) + (r * 3) + (n)] == 1) {
-                        tempNode.style.backgroundColor = "#6643DA";
-                    } else {
-                        tempNode.style.backgroundColor = "#FDC955";
-                    }
-                } else {
-                    tempNode.onclick = function () { setTeleNode(this.id) }
-                }
-                tempRow.appendChild(tempNode);
-            }
-            tempSection.appendChild(tempRow);
-        }
-        teleGridContainer.appendChild(tempSection);
-    }
-}
+// ---- SELECT CALLBACKS ----
 
 allianceSelect.addEventListener("change", function () {
     if (document.getElementById("alliance-select").value.substring(0, 1) == "R") {
@@ -353,29 +181,6 @@ autoTechCheck.addEventListener("click", function () {
         checkbox.style.backgroundColor = "rgb(93, 94, 95)";
         tl.to(checkbox, { scale: 0.75, duration: 0.15, ease: "power2" });
         tl.to(checkbox, { scale: 1, duration: 0.15, ease: "power2" });
-    }
-});
-
-autoChargeCheck.addEventListener("click", async function () {
-    autoCharge = !autoCharge;
-    var checkbox = document.getElementById("auto-charge-checkbox");
-
-    if (autoCharge) {
-        checkbox.style.backgroundColor = "#6feb36";
-        tl.to(checkbox, { scale: 1.25, duration: 0.15, ease: "power2" });
-        tl.to(checkbox, { scale: 1, duration: 0.15, ease: "power2" });
-
-        autoChargeOutcomeSelect.style.display = "flex";
-        tl.fromTo(autoChargeOutcomeSelect, { scale: 0, opacity: 0 }, { scale: 1, opacity: 1, duration: 0.5, ease: "power2" });
-    } else {
-        checkbox.style.backgroundColor = "rgb(93, 94, 95)";
-        tl.to(checkbox, { scale: 0.75, duration: 0.15, ease: "power2" });
-        tl.to(checkbox, { scale: 1, duration: 0.15, ease: "power2" });
-
-        tl.fromTo(autoChargeOutcomeSelect, { scale: 1, opacity: 1 }, { scale: 0, opacity: 0, duration: 0.5, ease: "power2" });
-
-        await sleep(500);
-        autoChargeOutcomeSelect.style.display = "none";
     }
 });
 
@@ -439,36 +244,6 @@ teleDisabledCheck.addEventListener("click", function () {
     }
 });
 
-teleChargeCheck.addEventListener("click", async function () {
-    teleCharge = !teleCharge;
-    var checkbox = document.getElementById("tele-charge-checkbox");
-
-    if (teleCharge) {
-        checkbox.style.backgroundColor = "#6feb36";
-        tl.to(checkbox, { scale: 1.25, duration: 0.15, ease: "power2" });
-        tl.to(checkbox, { scale: 1, duration: 0.15, ease: "power2" });
-
-        teleChargeContainer.style.display = "flex";
-        tl.fromTo(teleChargeContainer, { scale: 0, opacity: 0 }, { scale: 1, opacity: 1, duration: 0.5, ease: "power2" });
-        tl.fromTo(teleParkCheck, { scale: 1, opacity: 1 }, { scale: 0, opacity: 0, duration: 0.5, ease: "power2" }, "-=0.5");
-        await sleep(100);
-        tl.fromTo(teleChargeContainer, { marginTop: "20vw" }, { marginTop: "0vw", duration: 0.5, ease: "power2" }, "-=0.2");
-        teleParkCheck.style.display = "none";
-    } else {
-        checkbox.style.backgroundColor = "rgb(93, 94, 95)";
-        tl.to(checkbox, { scale: 0.75, duration: 0.15, ease: "power2" });
-        tl.to(checkbox, { scale: 1, duration: 0.15, ease: "power2" });
-
-        teleParkCheck.style.display = "flex";
-
-        tl.fromTo(teleChargeContainer, { scale: 1, opacity: 1 }, { scale: 0, opacity: 0, duration: 0.5, ease: "power2" });
-        tl.fromTo(teleParkCheck, { scale: 0, opacity: 0 }, { scale: 1, opacity: 1, duration: 0.5, ease: "power2" }, "-=0.5");
-
-        await sleep(500);
-        teleChargeContainer.style.display = "none";
-    }
-});
-
 teleParkCheck.addEventListener("click", function () {
     telePark = !telePark;
     var checkbox = document.getElementById("tele-park-checkbox");
@@ -514,7 +289,7 @@ recklessCheck.addEventListener("click", function () {
     }
 });
 
-// DROPPED PIECES
+// DROPPED PIECES AUTO update callback
 function autoDroppedPieceUpdate(num) {
     if(autoDroppedPieced + num < 0) {
         return;
@@ -523,6 +298,7 @@ function autoDroppedPieceUpdate(num) {
     autoDroppedPiecesText.innerText = `Missed Pieces: ${autoDroppedPieced}`
 }
 
+// DROPPED PIECES TELE update callback
 function teleDroppedPieceUpdate(num) {
     if(teleDroppedPieced + num < 0) {
         return;
@@ -531,17 +307,14 @@ function teleDroppedPieceUpdate(num) {
     teleDroppedPiecesText.innerText = `Missed Pieces: ${teleDroppedPieced}`
 }
 
+// Sleep function, pause/delay
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-windowResized();
-
+// Keeps the screen on apparently, may or may not work
 async function keepScreenOn() {
-    // Create a reference for the Wake Lock.
-    let wakeLock = null;
-
-    // create an async function to request a wake lock
+    // Create an async function to request a wake lock
     try {
         wakeLock = await navigator.wakeLock.request("screen");
     } catch (err) {
